aefc73bc1b2caefd3061b59c1ed9d76acabcc459
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2021-06-20 23:34:44 +0000

    Home UI

---
+++ lib/views/Home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'Dashboard.dart';
+
+class Home extends StatefulWidget {
+  static const routeName = '/home';
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int routeIndex = 0;
+  List<Widget> _routes = [
+    Dashboard(),
+  ];
+
+  void _onTabItemTapped(int index) {
+    routeIndex = index;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async { return Future.value(false); },
+      child: Scaffold(
+        body: Container(
+          child: _routes[routeIndex],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          unselectedIconTheme: IconThemeData(
+            color: Colors.grey
+          ),
+          elevation: 6,
+          backgroundColor: Theme.of(context).primaryColor,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_outlined),
+              label: 'Dashboard'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined),
+              label: 'Settings'
+            )
+          ],
+          currentIndex: routeIndex,
+          selectedItemColor: Theme.of(context).accentColor,
+          onTap: _onTabItemTapped,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
