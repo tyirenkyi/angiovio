caccbd3f60956ddaf550542736aed6ff9472c1ab
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2021-08-08 02:52:36 +0000

    Handle backpress in auth screens

--- lib/views/Login.dart
+++ lib/views/Login.dart
@@ -13,29 +13,32 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(33, 33, 33, 1),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.only(top: 80),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
+    return WillPopScope(
+      onWillPop: () async { return Future.value(false); },
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(33, 33, 33, 1),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(top: 80),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: EdgeInsets.only(top: 40),
+                  child: Image.asset('assets/images/logo.png'),
                 ),
-                margin: EdgeInsets.only(top: 40),
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              LoginForm()
-            ],
+                LoginForm()
+              ],
+            ),
           ),
-        ),
-      )
+        )
+      ),
     );
   }
 }
--- lib/views/SignUp.dart
+++ lib/views/SignUp.dart
@@ -12,29 +12,32 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(33, 33, 33, 1),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.only(top: 40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
+    return WillPopScope(
+      onWillPop: () async { return Future.value(false); },
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(33, 33, 33, 1),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    margin: EdgeInsets.only(top: 40),
+                    child: Image.asset('assets/images/logo.png'),
                   ),
-                  margin: EdgeInsets.only(top: 40),
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                SignUpForm()
-              ],
+                  SignUpForm()
+                ],
+              ),
             ),
-          ),
-        )
+          )
+      ),
     );
   }
 }
